96b7ac22a9d062a95acc28c2296f196383d77f8e
Harsha <hirushanharsha4@gmail.com>
2025-12-11 10:47:43 +0900

    added

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# glowguard_api.py
+# main code
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import chromadb
 from sentence_transformers import SentenceTransformer
 import json
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -93,4 +93,5 @@ def health():
     return jsonify({"status": "GlowGuard API is running!"})
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
